067460322b03e7e5b5643e013fcc7887274b0382
Syeda Nazish Adiba Khateeb <adibakhatib07@gmail.com>
2026-09-14 16:29:26 +0530

    Deploy_path_

--- app.py
+++ app.py
@@ -1,44 +1,18 @@
-# import streamlit as st 
-# from verifer_model import detect_news,accuracy
 
-# st.set_page_config(
-#     page_title="FAKE NEWS DETECTOR",
-#     page_icon="💯/❌",
-#     layout="centered")
-
-# st.title("FAKE NEWS DETECTION SYSTEM")
-# st.write("Enter a news article below & the system will analyze it as Fake or Real")
-
-# st.metric(label="Model accuracy",
-#           value= f"{accuracy*100:.2f}%")
-
-# news= st.text_area(
-#     "Enter News Article",
-#     height=250,
-#     placeholder="paste news article here or the title"
-# )
-
-# if st.button("DETECT NEWS",use_container_width=True):
-#     if not news.strip():
-#         st.warning("please enter a news article.")
-#     else:
-#         prediction,confidence= detect_news(news)
-#         if prediction == "Fake":
-#             st.error("Fake News, please fact check it")
-#         else:
-#             st.success("Source varified, Real News confirm")
-
-#         st.write(
-#             f"**Confidence:**{confidence:.2f}%"
-#         )
 
 from flask import  Flask, render_template, request
 import joblib
+import os
 
 app = Flask(__name__)
 
-model = joblib.load("model/VERIFIER LENSE MODEL.pkl")
-vectorizer = joblib.load("model/TF-IDF_VECTORIZIER.pkl")
+BASE_DIR =os.path.dirname(os.path.abspath(__file__))
+model= joblib.load(
+    os.path.join("model/VERIFIER LENSE MODEL.pkl")
+)
+
+vectorizer = joblib.load(
+    os.path.join(BASE_DIR,"model","TF-IDF_VECTORIZIER.pkl"))
 
 @app.route("/")
 def home():
